bbb4dc40a7c9dd61cdd1d2e01b0f29fdff2c4947
Valerio Gheri <valerio.gheri@gmail.com>
2012-05-23 17:01:51 +0200

    Initial commit
    Basic setup of the project

---
+++ iLikeIt/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iLikeIt.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Vote(string votedImages)
+        {
+            var images = new List<string>();
+            string[] split = votedImages.Split(',');
+            foreach (var src in split)
+            {
+                images.Add(src);
+            }
+        }   
+    }
+}
---
+++ iLikeIt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="iLikeIt.MvcApplication" Language="C#" %>
